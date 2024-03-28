28b4935dde51f82794bcc628070ed4e42e1a9fea
alex <starbucksisreallygood@gmail.com>
2024-03-28 13:23:20 -0700

    update test description for test check geometry type

--- tests/testthat/test-check_geometry_type.R
+++ tests/testthat/test-check_geometry_type.R
@@ -1,10 +1,10 @@
-test_that("Test the geometry check for case that should pass.", {
+test_that("geometry check passes when it should", {
   sf_object <- sf::st_read(test_path("data", "neftegorsk_ruptures.shp"), quiet = TRUE)
   expect_output(check_geometry_type(sf_object, "line"), "Geometry check passed.")
 })
 
 
-test_that("Test the geometry check for case that should return an error.", {
+test_that("geometry check returns an error when it should", {
   expect_error({
     sf_object <- sf::st_read(test_path("data", "neftegorsk_ruptures.shp"), quiet = TRUE)
     check_geometry_type(sf_object, "point")
