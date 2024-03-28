7611357d434d5501ae820cf21e633f712024b061
alex <starbucksisreallygood@gmail.com>
2024-03-28 15:23:31 -0700

    fix typo in test for porject to 4326

--- tests/testthat/test-project_to_4326.R
+++ tests/testthat/test-project_to_4326.R
@@ -1,13 +1,13 @@
 test_that("the coordinate reference system is converted to EPSG:4326", {
   sf_object <- sf::st_read(test_path("data", "example_ruptures_epsg32611.shp"), quiet = TRUE)
   sf_object_converted <- project_to_4326(sf_object)
-  crs <- sf::st_crs(blah)$epsg
+  crs <- sf::st_crs(sf_object_converted)$epsg
   expect_equal(crs, 4326)
 })
 
 test_that("the coordinate reference system remains EPSG:4326", {
   sf_object <- sf::st_read(test_path("data", "neftegorsk_ruptures_epsg4326.shp"), quiet = TRUE)
   sf_object_converted <- project_to_4326(sf_object)
-  crs <- sf::st_crs(blah)$epsg
+  crs <- sf::st_crs(sf_object_converted)$epsg
   expect_equal(crs, 4326)
 })
